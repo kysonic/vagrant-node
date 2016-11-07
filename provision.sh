70c5a3186c10bd3cca40261b3321403879d07156
Yuriy Sannikov <ysannikov@fonteva.com>
2016-11-07 14:06:12 -0500

    nodejs/nvm vagrant provisioning

--- provision.sh
+++ provision.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-sudo echo "Europe/Berlin" | sudo tee /etc/timezone
+sudo echo "Europe/Moscow" | sudo tee /etc/timezone
 sudo dpkg-reconfigure -f noninteractive tzdata
 
 sudo apt-get update -y
-sudo apt-get install -y build-essential curl git libssl-dev man mongodb-clients
+sudo apt-get install -y build-essential curl git libssl-dev man
 
-git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+
 echo "source ~/.nvm/nvm.sh" >> ~/.profile
+
 source ~/.profile
 
-nvm install 0.10
-nvm alias default 0.10
+nvm install v6.9.1
+nvm use v6.9.1
+nvm alias default v6.9.1
 
-npm install -g browserify
 npm install -g eslint
-npm install -g find-versions
-npm install -g forany
-npm install -g grunt-cli
-npm install -g harp
-npm install -g http-server
-npm install -g knockat
+npm install -g live-server
+npm install -g webpack
+npm install -g gulp
 npm install -g less
-npm install -g mocha
-npm install -g pm2
-npm install -g pm2-web
-npm install -g reqd
-npm install -g uglify-js
 
-echo "cd ~/projects" >> ~/.profile
+mkdir -p ~/repo
+echo "cd ~/repo" >> ~/.profile
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
