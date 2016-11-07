0258da2a6e17f9b6cccdc51a50421f97ca54f7bd
Yuriy Sannikov <ysannikov@fonteva.com>
2016-11-07 16:45:33 -0500

    add bower & instructions

--- provision.sh
+++ provision.sh
@@ -4,7 +4,9 @@ sudo echo "Europe/Moscow" | sudo tee /etc/timezone
 sudo dpkg-reconfigure -f noninteractive tzdata
 
 sudo apt-get update -y
-sudo apt-get install -y build-essential curl git libssl-dev man
+sudo apt-get install -y build-essential curl git libssl-dev man xclip
+
+git config --global core.editor "vim" 
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 
@@ -21,6 +23,7 @@ npm install -g live-server
 npm install -g webpack
 npm install -g gulp
 npm install -g less
+npm install -g bower
 
 mkdir -p ~/repo
 echo "cd ~/repo" >> ~/.profile
@@ -29,3 +32,10 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 echo ""
 echo "Everything done, have a nice day :-)!"
+echo ""
+echo "Do not forget to set up GIT:"
+echo ""
+echo "$ git config --global user.name \"John Doe\""
+echo "$ git config --global user.email johndoe@example.com"
+echo "Then set up GITHUB: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#platform-linux"
+
