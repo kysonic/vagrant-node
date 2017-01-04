85f8ba92dc1d0c5c3e7218b8fb17b01fea3b5e14
KySonic <soooyc@gmail.com>
2017-01-04 15:31:19 +0300

    Upgrade

---
+++ Vagrantfile
@@ -0,0 +1,32 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.require_version ">= 1.6.5"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.box_url = "https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20160215.0.0/providers/virtualbox.box"
+  config.vm.box_check_update = true
+
+  # Ports for http, https, Express, http-server and Harp
+	
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 3030, host: 3030
+  config.vm.network "forwarded_port", guest: 3031, host: 3031
+  config.vm.network "forwarded_port", guest: 3032, host: 3032
+
+  config.ssh.forward_agent = true
+
+  config.vm.synced_folder "../", "/home/vagrant/repo", fsnotify: [:modified]
+
+  config.vm.provider "virtualbox" do |vm|
+    vm.memory = 2048
+    vm.cpus = 2
+  end
+
+  config.vm.provider "vmware_fusion" do |vm|
+    vm.memory = 2048
+    vm.cpus = 2
+  end
+
+  config.vm.provision "shell", path: "provision.sh", privileged: false
+end
---
+++ provision.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+sudo echo "Europe/Moscow" | sudo tee /etc/timezone
+sudo dpkg-reconfigure -f noninteractive tzdata
+
+sudo apt-get update -y
+sudo apt-get install -y build-essential curl git libssl-dev man xclip libfuse-dev libcurl4-openssl-dev libxml2-dev mime-support automake libtool pkg-config unzip
+
+# Build s3fs-fuse
+mkdir -p ~/repo && cd "$_"
+git clone https://github.com/s3fs-fuse/s3fs-fuse
+cd s3fs-fuse/
+./autogen.sh
+./configure --prefix=/usr --with-openssl
+make
+sudo make install
+cd ~/repo && rm -rf ~/repo/s3fs-fuse
+
+# AWS tools
+mkdir -p ~/repo/aws && cd "$_"
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+cd ~/repo && rm -rf ~/repo/aws
+
+
+git config --global core.editor "vim" 
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+
+echo "source ~/.nvm/nvm.sh" >> ~/.profile
+
+source ~/.profile
+
+nvm install v6.9.1
+nvm use v6.9.1
+nvm alias default v6.9.1
+
+npm install -g eslint
+npm install -g babel-eslint
+npm install -g eslint-config-standard-react
+npm install -g live-server
+npm install -g concurrently
+npm install -g webpack
+npm install -g gulp
+npm install -g less
+npm install -g bower
+npm install -g better-npm-run
+
+mkdir -p ~/repo
+echo "cd ~/repo" >> ~/.profile
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+echo ""
+echo "Everything done, have a nice day :-)!"
+echo ""
+echo "Do not forget to set up GIT:"
+echo ""
+echo "$ git config --global user.name \"John Doe\""
+echo "$ git config --global user.email johndoe@example.com"
+echo "Then set up GITHUB: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#platform-linux"
+
